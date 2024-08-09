308659c6d973fad087adb748ece1e56a9ccbbce8
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-09 18:22:28 +0200

    Add script to only print given or default message

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
   def input(args)
-    print args[:message]
-    puts "Choose one of the options:"
+    message = args.fetch(:message, "Choose one of the options:")
+    puts message
   end
 end
