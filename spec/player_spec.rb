ba6621d48a101a60d8d2479e99a952b1885e0398
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-09 17:58:20 +0200

    Add test for default message for the input method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,12 @@ require_relative "../lib/player"
 require "rspec"
 
 RSpec.describe Player do
+  describe "#input" do
+    context "does it send a message to the player, describing what it wants" do
+      it "prints out a default message" do
+        default_message = /Choose one of the options:/
+        expect { subject.input }.to output(default_message).to_stdout
+      end
+    end
+  end
 end
