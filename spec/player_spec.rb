2a9c9a48c5d53ae07c5a686982cbcaa57c1d2d28
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-09 18:03:57 +0200

    Fix grammatical error

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,7 @@ require "rspec"
 
 RSpec.describe Player do
   describe "#input" do
-    context "does it send a message to the player, describing what it wants" do
+    context "does it send a message to the player describing what it wants" do
       it "prints out a default message" do
         default_message = /Choose one of the options:/
         expect { subject.input }.to output(default_message).to_stdout
