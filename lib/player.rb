4aa3f6420cbfa6a4837bdcfe7c0d6444ed61a0dc
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-09 21:38:40 +0200

    Add script for printing all input options

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,6 @@ class Player
   def input(args)
     message = args.fetch(:message, "Choose one of the options:")
     puts message
-    puts "yes"
-    puts "no"
+    args.fetch(:options, %w[yes no]).each { |option| puts option }
   end
 end
