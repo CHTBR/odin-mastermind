58f2252c6d5b4976ef1c2bcb50177f2718331b78
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-09 18:24:05 +0200

    Refactor test code and fix grammar

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,19 +4,18 @@ require "rspec"
 RSpec.describe Player do
   describe "#input" do
     context "does it send a message to the player describing what it wants" do
-      it "prints out a default message" do
-        default_message = /Choose one of the options:/
+      default_message = /Choose one of the options:/
+      message = "Choose yes or no:"
+
+      it "prints a default message" do
         expect { subject.input({}) }.to output(default_message).to_stdout
       end
 
-      it "prints out message given as argument" do
-        message = "Choose yes or no:"
+      it "prints message given as argument" do
         expect { subject.input({ message: message }) }.to output(Regexp.new(message)).to_stdout
       end
 
       it "prints only message given as argument" do
-        message = "Choose yes or no:"
-        default_message = /Choose one of the options:/
         expect { subject.input({ message: message }) }.to_not output(default_message).to_stdout
       end
     end
