f5c3447a0bb5f8576d70bc83bbf05ee87e285697
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-09 18:18:11 +0200

    Fix test failing because of a missing argument

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Player do
     context "does it send a message to the player describing what it wants" do
       it "prints out a default message" do
         default_message = /Choose one of the options:/
-        expect { subject.input }.to output(default_message).to_stdout
+        expect { subject.input({}) }.to output(default_message).to_stdout
       end
 
       it "prints out only message given as argument" do
