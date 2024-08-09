32b67e14ff7d5159012b3f8811a65132a6c9eef0
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-09 21:51:38 +0200

    Add test for repeating prompt till correct input

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -40,6 +40,12 @@ RSpec.describe Player do
         allow_any_instance_of(Object).to receive(:gets).and_return("up")
         expect(subject.input({ options: options })).to eql("up")
       end
+
+      it "prompts you again in case of incorrect input" do
+        options = %w[left right up down]
+        allow_any_instance_of(Object).to recieve(:gets).and_return("bogus input", "left")
+        expect(subject.input({ options: options })).to eql("left")
+      end
     end
   end
 end
