96508bc20ec2963d77309d46b635969ac396751f
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-10 08:11:24 +0200

    Add test for ModeDouble

--- spec/game_startet_spec.rb
+++ spec/game_startet_spec.rb
@@ -1,5 +1,12 @@
 require "rspec"
 require_relative "../lib/game_starter"
+require_relative "startable_spec"
+
+RSpec.describe ModeDouble do
+  it_behaves_like "a startable" do
+    let(:subject) { ModeDouble.new }
+  end
+end
 
 RSpec.describe GameStarter do
   subject { GameStarter.new({ input_manager: InputManagerDouble.new, mode_list: [ModeDouble.new, ModeDouble.new] }) }
