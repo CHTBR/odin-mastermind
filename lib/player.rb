159bd9e12936cb340b1dd433960a791fcea1c4a8
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-10 07:03:35 +0200

    Refactor code for single purpose methods

--- lib/player.rb
+++ lib/player.rb
@@ -2,8 +2,20 @@ class Player
   def input(args)
     message = args.fetch(:message, "Choose one of the options:")
     options = args.fetch(:options, %w[yes no])
+    _print_message(message)
+    _print_each_option(options)
+    _start_input_loop(options)
+  end
+
+  def _print_message(message)
     puts message
-    options.each { |option| puts option }
+  end
+
+  def _print_each_option(options)
+    options.each { |option| puts "* #{option}" }
+  end
+
+  def _start_input_loop(options)
     loop do
       input = gets.chomp
       return input if options.include?(input)
