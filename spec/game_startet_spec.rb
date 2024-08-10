b573b6a94b7b0d33056f27738e3d3608037e3bbe
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-10 08:07:01 +0200

    Add test init arguments for GameStarter

--- spec/game_startet_spec.rb
+++ spec/game_startet_spec.rb
@@ -2,4 +2,5 @@ require "rspec"
 require_relative "../lib/game_starter"
 
 RSpec.describe GameStarter do
+  subject { GameStarter.new({ input_manager: InputManagerDouble.new, mode_list: [ModeDouble.new, ModeDouble.new] }) }
 end
