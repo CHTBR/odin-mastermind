1b05bfa7228a92a70dfe77ddd940da0b66cde51b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-10 08:12:23 +0200

    Add a ModeDOuble class that behaves like startable

--- spec/game_startet_spec.rb
+++ spec/game_startet_spec.rb
@@ -2,6 +2,11 @@ require "rspec"
 require_relative "../lib/game_starter"
 require_relative "startable_spec"
 
+class ModeDouble
+  def start
+  end
+end
+
 RSpec.describe ModeDouble do
   it_behaves_like "a startable" do
     let(:subject) { ModeDouble.new }
