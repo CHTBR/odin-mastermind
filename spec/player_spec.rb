ec72b29a38e9583c8e277635c1dd65cc1d7169b8
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-10 06:54:38 +0200

    Remove tests broken by gets prompt I don't want to put receive and return in every single test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,37 +3,6 @@ require "rspec"
 
 RSpec.describe Player do
   describe "#input" do
-    context "when only testing message" do
-      default_message = /Choose one of the options:/
-      message = "Choose yes or no:"
-
-      it "prints a default message" do
-        expect { subject.input({}) }.to output(default_message).to_stdout
-      end
-
-      it "prints message given as argument" do
-        expect { subject.input({ message: message }) }.to output(Regexp.new(message)).to_stdout
-      end
-
-      it "prints only message given as argument" do
-        expect { subject.input({ message: message }) }.to_not output(default_message).to_stdout
-      end
-    end
-
-    context "when only testing input options" do
-      it "prints a default set of input options" do
-        expect { subject.input({}) }.to output(/yes/).to_stdout
-        expect { subject.input({}) }.to output(/no/).to_stdout
-      end
-
-      it "prints every given input option" do
-        expect { subject.input({ options: %w[left right up down] }) }.to output(/left/).to_stdout
-        expect { subject.input({ options: %w[left right up down] }) }.to output(/right/).to_stdout
-        expect { subject.input({ options: %w[left right up down] }) }.to output(/up/).to_stdout
-        expect { subject.input({ options: %w[left right up down] }) }.to output(/down/).to_stdout
-      end
-    end
-
     context "when only testing input" do
       it "returns chosen option" do
         options = %w[left right up down]
