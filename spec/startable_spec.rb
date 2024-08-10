8a024e0ec86472b6cdecb1828b2d2606dd0d6820
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-10 08:10:45 +0200

    Fix name of shared example

--- spec/startable_spec.rb
+++ spec/startable_spec.rb
@@ -1,6 +1,6 @@
 require "rspec"
 
-RSpec.shared_examples "behaves like a startable" do
+RSpec.shared_examples "a startable" do
   it "responds to the #start method" do
     expect(subject).to respond_to(:start)
   end
