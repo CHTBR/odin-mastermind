ca67218d8782841d2f18250b4b2973effea5467b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-10 20:56:02 +0200

    Replace traditional double with rspec double

--- spec/game_startet_spec.rb
+++ spec/game_startet_spec.rb
@@ -1,15 +1,10 @@
 require "rspec"
 require_relative "../lib/game_starter"
+require_relative "../lib/player"
 require_relative "startable_spec"
-require_relative "input_manager_spec"
 
 class ModeDouble
   def start
-  end
-end
-
-class InputManagerDouble
-  def input
   end
 end
 
@@ -19,18 +14,7 @@ RSpec.describe ModeDouble do
   end
 end
 
-RSpec.describe InputManagerDouble do
-  it_behaves_like "an input manager" do
-    let(:subject) { InputManagerDouble.new }
-  end
-
-  describe "#input" do
-    it "returns string 'input'" do
-      expect(subject.input).to eql("input")
-    end
-  end
-end
-
 RSpec.describe GameStarter do
-  subject { GameStarter.new({ input_manager: InputManagerDouble.new, mode_list: [ModeDouble.new, ModeDouble.new] }) }
+  let(:input_manager) { instance_double("Player") }
+  subject { GameStarter.new({ input_manager: input_manager, mode_list: [ModeDouble.new, ModeDouble.new] }) }
 end
