eb589ae8b6a5a184b3f537f248e5ac877e51260b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 09:00:52 +0200

    Add class init before every test

--- spec/game_startet_spec.rb
+++ spec/game_startet_spec.rb
@@ -3,18 +3,14 @@ require_relative "../lib/game_starter"
 require_relative "../lib/player"
 require_relative "startable_spec"
 
-class ModeDouble
-  def start
-  end
-end
-
-RSpec.describe ModeDouble do
-  it_behaves_like "a startable" do
-    let(:subject) { ModeDouble.new }
-  end
-end
-
 RSpec.describe GameStarter do
-  let(:input_manager) { instance_double("Player") }
-  subject { GameStarter.new({ input_manager: input_manager, mode_list: [ModeDouble.new, ModeDouble.new] }) }
+  before(:example) do
+    input_manager_double = instance_double("Player", :input)
+    startable_double1 = double("Startable Double 1")
+    allow(startable_double1).to receive(:start)
+    startable_double2 = double("Startable Double 2")
+    allow(startable_double2).to receive(:start)
+    game_starter = GameStarter.new({ input_manager: input_manager_double,
+                                     mode_list: [startable_double1, startable_double2] })
+  end
 end
