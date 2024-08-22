9336e5c88956a2ffb7a8cf4c3c49a36aee7116cd
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 09:44:51 +0200

    Add script for calling given game mode

--- lib/game_starter.rb
+++ lib/game_starter.rb
@@ -5,6 +5,7 @@ class GameStarter
   end
 
   def start_game
-    @input_manager.input({ message: "Select game mode:", options: @mode_list })
+    selected_game_mode = @input_manager.input({ message: "Select game mode:", options: @mode_list })
+    selected_game_mode.start
   end
 end
