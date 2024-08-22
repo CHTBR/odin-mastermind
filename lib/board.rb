6c5407a5a262b7daaf3e81671df9a97bc26c0104
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 12:01:16 +0200

    Add colors to ASCII board representantion

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,11 @@
+require "colorize"
+
 class Board
   attr_accessor :target
   attr_reader :guesses
 
   def initialize
-    @guesses = Array.new(12) { Array.new(4) }
+    @guesses = Array.new(12) { Array.new(4, :default) }
   end
 
   def set_column(args)
@@ -11,16 +13,23 @@ class Board
   end
 
   def to_s
-    "
+    string = "
         _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-       |     | :: :: :: :: :: :: :: :: :: :: :: ::   |
-       |  ?  |  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^   |
-       |  ?  |  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^   |
-       |  ?  |  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^   |
-       |  ?  |  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^   |
-       |_ _ _|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+       |     | :: :: :: :: :: :: :: :: :: :: :: ::   |\n"
+    4.times do |row|
+      string << "       |  "
+      string << (target.nil? ? "?" : "^".colorize(target[row]))
+      string << "  |"
+      12.times do |column|
+        string << "  "
+        string << "^".colorize(@guesses[column][row])
+      end
+      string << "   |\n"
+    end
+    string << "       |_ _ _|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
         \\ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ /
 
 "
+    string
   end
 end
