9eb9f043355a7049a37cc4d77a74a3d91fbb435f
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 17:12:57 +0200

    Add test for target acccessors

--- spec/guess_evaluator_spec.rb
+++ spec/guess_evaluator_spec.rb
@@ -2,7 +2,13 @@ require "rspec"
 require_relative "../lib/guess_evaluator"
 
 RSpec.describe GuessEvaluator do
-  describe "#set_target" do
+  describe "#target=" do
+    context "when given a target sequence" do
+      xit "saves it into a target variable" do
+        subject.target = %i[red red red blue]
+        expect(subject.target).to eql(%i[red red red blue])
+      end
+    end
   end
 
   describe "#evaluate_guess" do
