ddb839ff66c9f157afb6318a9230dd393f3369ef
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 09:14:17 +0200

    Fix variable names to make them available for use

--- spec/game_startet_spec.rb
+++ spec/game_startet_spec.rb
@@ -5,30 +5,30 @@ require_relative "startable_spec"
 
 RSpec.describe GameStarter do
   before(:example) do
-    input_manager_double = instance_double("Player", :input)
-    startable_double1 = double("Startable Double 1")
-    allow(startable_double1).to receive(:start)
-    startable_double2 = double("Startable Double 2")
-    allow(startable_double2).to receive(:start)
-    game_starter = GameStarter.new({ input_manager: input_manager_double,
-                                     mode_list: [startable_double1, startable_double2] })
+    @input_manager_double = instance_double("Player", :input)
+    @startable_double1 = double("Startable Double 1")
+    allow(@startable_double1).to receive(:start)
+    @startable_double2 = double("Startable Double 2")
+    allow(@startable_double2).to receive(:start)
+    @game_starter = GameStarter.new({ input_manager: @input_manager_double,
+                                      mode_list: [@startable_double1, @startable_double2] })
   end
 
   describe "#start_game" do
     it "responds to #start_game" do
-      expect(game_starter).to respond_to(:start_game)
+      expect(@game_starter).to respond_to(:start_game)
     end
 
     xit "calls the start method of startable_double1 when given by input_manager" do
-      allow(input_manager).to receive(input: startable_double1)
-      expect(startable_double1).to receive(:start)
-      game_starter.start_game
+      allow(@input_manager).to receive(input: @startable_double1)
+      expect(@startable_double1).to receive(:start)
+      @game_starter.start_game
     end
 
     xit "calls the start method of startable_double2 when given by input_manager" do
-      allow(input_manager).to receive(input: startable_double2)
-      expect(startable_double2).to receive(:start)
-      game_starter.start_game
+      allow(@input_manager).to receive(input: @startable_double2)
+      expect(@startable_double2).to receive(:start)
+      @game_starter.start_game
     end
   end
 end
