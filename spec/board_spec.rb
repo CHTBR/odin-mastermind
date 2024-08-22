bb0ee67d4d906e9d880c28043557d7c3f716a889
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 10:38:37 +0200

    Add test file for Board with defined methods

---
+++ spec/board_spec.rb
@@ -0,0 +1,13 @@
+require "rspec"
+require_relative "../lib/board"
+
+RSpec.describe Board do
+  descibe "#set_target" do
+  end
+
+  describe "#set_column" do
+  end
+
+  describe "#to_s" do
+  end
+end
