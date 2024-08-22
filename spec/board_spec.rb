3851fd6796cc284775834de2c1945b64261575a5
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 11:00:59 +0200

    Add test for guesses setter

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe Board do
   end
 
   describe "#set_column" do
+    it "sets the 1st column to the given sequence" do
+      subject.set_column({ number: 1, sequence: %i[b g y y] })
+      expect(subject.guesses[0]).to eql(%i[b g y y])
+    end
+
+    it "sets the 12th column to the given sequence" do
+      subject.set_column({ number: 12, sequence: %i[g r g y] })
+      expect(subject.guesses[11]).to eql(%i[g r g y])
+    end
   end
 
   describe "#to_s" do
