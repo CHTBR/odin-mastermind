2e231b8fab8ceab3a1524c736d9522cf4b109d19
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-22 11:36:55 +0200

    Fix input sequence color names

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe Board do
 
   describe "#target" do
     it "sets the target sequence to the given input" do
-      subject.target = %i[r g b y]
-      expect(subject.target).to eql(%i[r g b y])
+      subject.target = %i[red green blue yellow]
+      expect(subject.target).to eql(%i[red green blue yellow])
     end
   end
 
   describe "#set_column" do
     it "sets the 1st column to the given sequence" do
-      subject.set_column({ number: 1, sequence: %i[b g y y] })
-      expect(subject.guesses[0]).to eql(%i[b g y y])
+      subject.set_column({ number: 1, sequence: %i[blue green yellow yellow] })
+      expect(subject.guesses[0]).to eql(%i[blue green yellow yellow])
     end
 
     it "sets the 12th column to the given sequence" do
-      subject.set_column({ number: 12, sequence: %i[g r g y] })
-      expect(subject.guesses[11]).to eql(%i[g r g y])
+      subject.set_column({ number: 12, sequence: %i[green red green yellow] })
+      expect(subject.guesses[11]).to eql(%i[green red green yellow])
     end
   end
 
