93532624e1500a776776876925d1e13f9c4d0ebd
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-23 15:52:45 +0200

    Fix variable names to make them available for use

--- spec/computer_mode_spec.rb
+++ spec/computer_mode_spec.rb
@@ -6,15 +6,15 @@ require_relative "../lib/guess_evaluator"
 
 RSpec.describe ComputerMode do
   before do
-    guess_evaluator_double = instance_double("GuessEvaluator")
-    board_double = instance_double("Board")
-    computer_double = double("Computer")
-    allow(computer_double).to receive(:guess)
-    computer_mode = ComputerMode.new({ guess_evaluator: guess_evaluator_double, board: board_double,
-                                       computer: computer_double })
+    @guess_evaluator_double = instance_double("GuessEvaluator")
+    @board_double = instance_double("Board")
+    @computer_double = double("Computer")
+    allow(@computer_double).to receive(:guess)
+    @computer_mode = ComputerMode.new({ guess_evaluator: @guess_evaluator_double, board: @board_double,
+                                        computer: @computer_double })
   end
 
   it_behaves_like "a startable" do
-    subject { ComputerMode.new({ guess_evaluator: "", board: "", computer: "" }) }
+    subject { @computer_mode }
   end
 end
