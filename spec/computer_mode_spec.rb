79a2938da9870fe2e9462a1c38bcf2567c81e371
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-24 16:44:21 +0200

    Fix tests failling bc of missing return values

--- spec/computer_mode_spec.rb
+++ spec/computer_mode_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe ComputerMode do
   before do
     @guess_evaluator_double = instance_double("GuessEvaluator")
     allow(@guess_evaluator_double).to receive(:target=)
-    allow(@guess_evaluator_double).to receive(:evaluate_guess)
+    allow(@guess_evaluator_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 })
     @board_double = instance_double("Board")
     allow(@board_double).to receive(:target=)
     allow(@board_double).to receive(:set_column)
@@ -65,7 +65,7 @@ RSpec.describe ComputerMode do
         allow(@board_double).to receive(:set_column)
       end
 
-      xit "sends a message to ask the computer for a guess every round and no more" do
+      it "sends a message to ask the computer for a guess every round and no more" do
         @computer_mode.start
         expect(@computer_double).to have_received(:input).exactly(7).times
       end
