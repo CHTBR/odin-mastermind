51eb59d0b0c79198781ec53a6a5948af88fd2f86
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-24 16:27:07 +0200

    Add script that asks computer for input 12 times

--- lib/computer_mode.rb
+++ lib/computer_mode.rb
@@ -2,11 +2,16 @@ class ComputerMode
   def initialize(args)
     @guess_evaluator = args[:guess_evaluator]
     @board = args[:board]
+    @computer = args[:computer]
   end
 
   def start
     target = Array.new(4).map { |_element| %i[red green red yellow].shuffle.shuffle.sample }
     @guess_evaluator.target = target
     @board.target = target
+
+    12.times do
+      @computer.input
+    end
   end
 end
