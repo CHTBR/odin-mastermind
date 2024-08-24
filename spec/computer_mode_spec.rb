7609e5ec3f8de9310185335242dcb2f129b25fcf
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-24 15:40:15 +0200

    Refactor test code

--- spec/computer_mode_spec.rb
+++ spec/computer_mode_spec.rb
@@ -34,30 +34,35 @@ RSpec.describe ComputerMode do
 
   context "during the full game consisting of at most 12 rounds" do
     context "when a computer doesn't guess the correct sequence" do
-      xit "sends a message to ask the computer for a guess every round and no more" do
+      before do
         allow(@input_manager_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 })
+      end
+
+      xit "sends a message to ask the computer for a guess every round and no more" do
         allow(@computer).to receive(:input)
         @computer_mode.start
         expect(@computer).to have_received(:input).exactly(12).times
       end
       xit "sends a message to update board every round" do
-        allow(@input_manager_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 })
         allow(@board).to receive(:set_column)
         @computer_mode.start
         expect(@board).to have_received(:set_column).exactly(12).times
       end
     end
+
     context "when a computer wins in the 7th round of the game" do
-      xit "sends a message to ask the computer for a guess every round and no more" do
+      before do
         allow(@input_manager_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 },
-                                                                            { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 4 })
+                                                                            { color: 0, color_and_spot: 0 }, { color: 0, color_andspot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 4 })
+      end
+
+      xit "sends a message to ask the computer for a guess every round and no more" do
         allow(@computer).to receive(:input)
         @computer_mode.start
         expect(@computer).to have_received(:input).exactly(7).times
       end
+
       xit "sends a message to update board every round" do
-        allow(@input_manager_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 },
-                                                                            { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 4 })
         allow(@board).to receive(:input)
         @computer_mode.start
         expect(@board).to have_received(:input).exactly(7).times
