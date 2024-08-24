eb8870e4c4651bbca097abe416cf546ffc8e59fe
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-24 16:21:21 +0200

    Fix variable names and expectations for tests

--- spec/computer_mode_spec.rb
+++ spec/computer_mode_spec.rb
@@ -35,37 +35,44 @@ RSpec.describe ComputerMode do
   context "during the full game consisting of at most 12 rounds" do
     context "when a computer doesn't guess the correct sequence" do
       before do
-        allow(@input_manager_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 })
+        allow(@computer_double).to receive(:input).and_return("board")
+        allow(@guess_evaluator_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 })
+        allow(@board_double).to receive(:set_column)
       end
 
       xit "sends a message to ask the computer for a guess every round and no more" do
-        allow(@computer).to receive(:input)
         @computer_mode.start
-        expect(@computer).to have_received(:input).exactly(12).times
+        expect(@computer_double).to have_received(:input).exactly(12).times
+      end
+      xit "sends a message to ask the guess_evaluator for a guess evaluation every round and no more" do
+        @computer_mode.start
+        expect(@guess_evaluator_double).to have_received(:evaluate_guess).wxith("board").exactly(12).times
       end
       xit "sends a message to update board every round" do
-        allow(@board).to receive(:set_column)
         @computer_mode.start
-        expect(@board).to have_received(:set_column).exactly(12).times
+        expect(@board_double).to have_received(:set_column).with("board").exactly(12).times
       end
     end
 
     context "when a computer wins in the 7th round of the game" do
       before do
-        allow(@input_manager_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 },
-                                                                            { color: 0, color_and_spot: 0 }, { color: 0, color_andspot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 4 })
+        allow(@computer_double).to receive(:input).and_return("board")
+        allow(@guess_evaluator_double).to receive(:evaluate_guess).and_return({ color: 0, color_and_spot: 0 },
+                                                                              { color: 0, color_and_spot: 0 }, { color: 0, color_andspot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 0 }, { color: 0, color_and_spot: 4 })
+        allow(@board_double).to receive(:set_column)
       end
 
       xit "sends a message to ask the computer for a guess every round and no more" do
-        allow(@computer).to receive(:input)
         @computer_mode.start
-        expect(@computer).to have_received(:input).exactly(7).times
+        expect(@computer_double).to have_received(:input).exactly(7).times
       end
-
-      xit "sends a message to update board every round" do
-        allow(@board).to receive(:input)
+      xit "sends a message to ask the guess_evaluator for a guess evaluation every round and no more" do
         @computer_mode.start
-        expect(@board).to have_received(:input).exactly(7).times
+        expect(@guess_evaluator_double).to have_received(:evaluate_guess).with("board").exactly(7).times
+      end
+      xit "sends a message to update board every round" do
+        @computer_mode.start
+        expect(@board_double).to have_received(:set_column).with("board").exactly(7).times
       end
     end
   end
