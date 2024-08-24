e7a253324b7da5a46c5e35e397ce8a014d3d40b4
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-24 19:37:56 +0200

    Refix decoupling the target from global

--- lib/guess_evaluator.rb
+++ lib/guess_evaluator.rb
@@ -2,7 +2,7 @@ class GuessEvaluator
   attr_accessor :target
 
   def evaluate_guess(guesses)
-    @target = Hash.new(:target)
+    @temporary_target = Array.new(target)
     evaluation = { color_and_spot: 0, color: 0 }
     _find_all_color_and_spot_matches(evaluation, guesses)
     _find_all_color_matches(evaluation, guesses)
@@ -11,20 +11,22 @@ class GuessEvaluator
   end
 
   def _find_all_color_and_spot_matches(evaluation, guesses)
-    target.each_with_index { |target_entry, index| evaluation[:color_and_spot] += 1 if guesses[index] == target_entry }
+    @temporary_target.each_with_index do |target_entry, index|
+      evaluation[:color_and_spot] += 1 if guesses[index] == target_entry
+    end
   end
 
   def _find_all_color_matches(evaluation, guesses)
     guesses.each do |guess|
-      if target.include?(guess)
+      if @temporary_target.include?(guess)
         evaluation[:color] += 1
-        _remove_one_object_from_array(target, guess)
+        _remove_one_object_from_array(@temporary_target, guess)
       end
     end
   end
 
   def _remove_one_object_from_array(array, object)
-    array.delete_at(target.index(object))
+    array.delete_at(array.index(object))
   end
 
   def _remove_duplicate_color_matches(evaluation)
