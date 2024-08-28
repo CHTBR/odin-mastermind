a39883f02cfb50bc1a69f456e62af55e1ec44ad2
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-28 10:30:18 +0200

    Add script for using defalt guess on first call

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,6 +1,14 @@
 class Computer
   def initialize(args)
+    @guess_evaluator = args[:guess_evaluator]
     @raw_guess = [0, 0, 0, 0]
+    @previous_evaluation = nil
+  end
+
+  def input
+    guess = _convert_raw_guess unless @previous_evaluation
+    @previous_evaluation = @guess_evaluator.evaluate_guess(guess)
+    guess
   end
 
   def _convert_raw_guess
