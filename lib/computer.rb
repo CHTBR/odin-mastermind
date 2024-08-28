6ab204cb8a95b4db76851271ac2d466435597c7b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-08-28 09:49:03 +0200

    Add a method for converting guess into expected format

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,4 +2,9 @@ class Computer
   def initialize(args)
     @raw_guess = [0, 0, 0, 0]
   end
+
+  def _convert_raw_guess
+    options = %i[red green blue yellow]
+    Array.new(4).map.with_index { |_element, index| options[@raw_guess[index]] }
+  end
 end
