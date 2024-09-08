10f6be57f75e36b31affb5b276cfe9ef40ad32c7
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-08 16:07:57 +0200

    Add object instance declarations with injected dependencies

--- main.rb
+++ main.rb
@@ -4,3 +4,11 @@ require_relative "lib/computer_mode"
 require_relative "lib/guess_evaluator"
 require_relative "lib/computer"
 require_relative "lib/board"
+
+input_manager = Player.new
+guess_evaluator = GuessEvaluator.new
+board = Board.new
+computer = Computer.new({ guess_evaluator: guess_evaluator })
+computer_mode = ComputerMode.new({ guess_evaluator: guess_evaluator, board: board, computer: computer,
+                                   input_manager: input_manager })
+game_starter = GameStarter.new({ input_manager: input_manager, mode_list: [computer_mode] })
