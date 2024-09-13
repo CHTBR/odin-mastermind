ebb236dfcb5114b6b8a152a7208746e5c49c66a2
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-13 21:36:08 +0200

    Remove skipping last option to lower complexity

--- lib/computer.rb
+++ lib/computer.rb
@@ -12,11 +12,6 @@ class Computer
     guess = _convert_raw_guess if _current_evaluation == -1
     # if we got the option right in this column and this is not one of the first two guesses
     @current_column += 1 if _current_evaluation == (_previous_evaluation + 1) && _previous_evaluation != -1
-    # if we're on the one before last option and our current evaluation isn't 0 and our evaluation isn't incorrect
-    if @raw_guess[@current_column] == 2 && _current_evaluation != 0 && _previous_evaluation <= _current_evaluation
-      _skip_last_option_and_move_to_next_column
-      guess = _convert_raw_guess
-    end
     # unless there's already a guess or or our current evaluation isn't 0 or all the columns don't have the same option
     unless guess || _current_evaluation != 0 || @raw_guess.uniq.size != 1
       _increment_all_columns
@@ -27,15 +22,6 @@ class Computer
     guess = _convert_raw_guess
     _evaluate_guess(guess)
     guess
-  end
-
-  def _skip_last_option_and_move_to_next_column
-    @raw_guess[@current_column] += 1
-    # if we aren't in the last column
-    return if @current_column == 3
-    @current_column += 1
-    @raw_guess[@current_column] += 1
-    @current_evaluation[:color_and_spot] += 1
   end
 
   def _increment_all_columns
