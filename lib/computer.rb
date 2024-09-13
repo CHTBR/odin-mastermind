7c8050f163e4b1cd9cb0192070ea81a5a3dda9ed
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-13 18:23:28 +0200

    Add a private accessor to shorten variable names

--- lib/computer.rb
+++ lib/computer.rb
@@ -8,29 +8,28 @@ class Computer
   end
 
   def input
-    guess = _convert_raw_guess if @current_evaluation[:color_and_spot] == -1
-    @current_column += 1 if @current_evaluation[:color_and_spot] == (@previous_evaluation[:color_and_spot] + 1) && @previous_evaluation[:color_and_spot] != -1
-    if @raw_guess[@current_column] == 2 && @current_evaluation[:color_and_spot] != 0 && !(@previous_evaluation[:color_and_spot] > @current_evaluation[:color_and_spot])
+    guess = _convert_raw_guess if _current_evaluation == -1
+    @current_column += 1 if _current_evaluation == (_previous_evaluation + 1) && _previous_evaluation != -1
+    if @raw_guess[@current_column] == 2 && _current_evaluation != 0 && !(_previous_evaluation > _current_evaluation)
       @raw_guess[@current_column] += 1
       unless @current_column == 3
         @current_column += 1
         @raw_guess[@current_column] += 1
         @current_evaluation[:color_and_spot] += 1
-        pp @current_evaluation[:color_and_spot] # test
       end
       guess = _convert_raw_guess
     end
-    unless guess || @current_evaluation[:color_and_spot] != 0 || @raw_guess.uniq.size != 1
+    unless guess || _current_evaluation != 0 || @raw_guess.uniq.size != 1
       @raw_guess = @raw_guess.map { |element| element + 1 }
       guess = _convert_raw_guess
     end
-    unless guess || @current_evaluation[:color_and_spot] == 4
-      if @previous_evaluation[:color_and_spot] > @current_evaluation[:color_and_spot]
+    unless guess || _current_evaluation == 4
+      if _previous_evaluation >_current_evaluation 
         @raw_guess[@current_column] -= 1
         @current_column += 1
         @current_evaluation[:color_and_spot] += 1
       end
-      @raw_guess[@current_column] += 1 unless @current_evaluation[:color_and_spot] == 4
+      @raw_guess[@current_column] += 1 unless _current_evaluation == 4
       guess = _convert_raw_guess
     end
     guess = _convert_raw_guess
@@ -42,5 +41,13 @@ class Computer
   def _convert_raw_guess
     options = %i[red green blue yellow]
     Array.new(4).map.with_index { |_element, index| options[@raw_guess[index]] }
+  end
+
+  def _current_evaluation
+    @current_evaluation[:color_and_spot]
+  end
+
+  def _previous_evaluation
+    @previous_evaluation[:color_and_spot]
   end
 end
