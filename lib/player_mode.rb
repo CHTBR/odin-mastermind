61fec9964d3cb99a838ba69c221d670ef86a8a38
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-14 12:05:36 +0200

    Add script to give guess evaluator a random target at the start

--- lib/player_mode.rb
+++ lib/player_mode.rb
@@ -1,6 +1,11 @@
 class PlayerMode
-  def initialize(args); end
-  def start; end
+  def initialize(args)
+    @guess_evaluator = args[:guess_evaluator]
+  end
+
+  def start
+    @guess_evaluator.target = Array.new(4) { %i[red green blue yellow].shuffle.shuffle.sample } # Use multiple shuffles to increase randomness
+  end
 
   def to_s
     "Player Mode"
