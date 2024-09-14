0d4bf17cf6dec692228f31f048032eacb32018e8
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-14 09:00:59 +0200

    Add test file with requires for PlayerMode

---
+++ spec/player_mode_spec.rb
@@ -0,0 +1,9 @@
+require "rspec"
+require_realtive "startable_spec"
+require_relative "../lib/player_mode"
+require_relative "../lib/player"
+require_relative "../lib/board"
+require_relative "../lib/guess_evaluator"
+
+RSpec.describe PlayerMode do
+end
