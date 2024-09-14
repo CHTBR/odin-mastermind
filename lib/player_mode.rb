98ef8ed15a27d79cefcd12ea54b9ba3725c009e9
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-14 12:58:42 +0200

    Add script to end game after player wins

--- lib/player_mode.rb
+++ lib/player_mode.rb
@@ -13,6 +13,7 @@ class PlayerMode
       evaluation = @guess_evaluator.evaluate_guess(guess)
       @board.set_column({ number: round, sequence: guess })
       round += 1
+      break if _won?(evaluation)
     end
   end
 
@@ -22,5 +23,9 @@ class PlayerMode
 
   def _player_guess
     Array.new(4) { @input_manager.input({ message: "Choosea color:", options: %i[red green blue yellow] }) }
+  end
+
+  def _won?(evaluation)
+    evaluation[:color_and_spot] == 4
   end
 end
