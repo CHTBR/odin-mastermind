f1e90b0b5c2c1cf58bf8cb7f34749dde8f3f6a8c
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-14 09:11:14 +0200

    Add PlayerMode initialization

--- spec/player_mode_spec.rb
+++ spec/player_mode_spec.rb
@@ -6,4 +6,10 @@ require_relative "../lib/board"
 require_relative "../lib/guess_evaluator"
 
 RSpec.describe PlayerMode do
+  before do
+    @player_double = instance_double("Player")
+    @board_double = instance_double("Board")
+    @guess_evaluator_double = instance_double("GuessEvaluator")
+    @player_mode = PlayerMode.new({ input_manager: @player_double, board: @board_double, guess_evaluator: @guess_evaluator })
+  end
 end
