62800c4cae1211d2941c7619ac4a8c750089776d
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-14 12:47:22 +0200

    Add script to ask for input 12 times

--- lib/player_mode.rb
+++ lib/player_mode.rb
@@ -1,13 +1,23 @@
 class PlayerMode
   def initialize(args)
     @guess_evaluator = args[:guess_evaluator]
+    @input_manager = args[:input_manager]
   end
 
   def start
     @guess_evaluator.target = Array.new(4) { %i[red green blue yellow].shuffle.shuffle.sample } # Use multiple shuffles to increase randomness
+    round = 0
+    until round == 12
+      guess = _player_guess
+      round += 1
+    end
   end
 
   def to_s
     "Player Mode"
+  end
+
+  def _player_guess
+    Array.new(4) { @input_manager.input({ message: "Choosea color:", options: %i[red green blue yellow] }) }
   end
 end
